336aab5d4d5b8539349dc7e0e8f075ce79175077
Ziyue Liu <zil085@ucsd.edu>
2024-02-17 20:14:36 -0800

    Tested RollOver, Finish the Date Mock

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -3,10 +3,13 @@ package edu.ucsd.cse110.successorator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -24,17 +27,20 @@ import edu.ucsd.cse110.successorator.ui.goallist.dialog.CreateGoalDialogFragment
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding view;
-    private MainViewModel model;
+    private MainViewModel activityModel;
     private GoalListFragment goalList;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.goalList = GoalListFragment.newInstance();
-      
+
+        var modelFactory = ViewModelProvider.Factory.from(MainViewModel.initializer);
+        var modelProvider = new ViewModelProvider(this, modelFactory);
+        this.activityModel = modelProvider.get(MainViewModel.class);
         //To be modified
-        DateFormat dateFormat = new SimpleDateFormat("EEEE, MM/dd");
-        Date date = new Date();
-        setTitle(dateFormat.format(date));
+        this.activityModel.getDateString().observe(dateString -> {
+            setTitle(dateString);
+        });
       
         //^^^
         getSupportFragmentManager()
@@ -43,18 +49,28 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
       
         this.view = ActivityMainBinding.inflate(getLayoutInflater());
-        this.model = new MainViewModel(((SuccessoratorApplication) getApplication()).getGoalRepository());
 
         setContentView(view.getRoot());
     }
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.editgoal, menu);
-        menu.findItem(R.id.edit_bar_menu_add_goal).setOnMenuItemClickListener(item -> {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        var itemId = item.getItemId();
+
+        if (itemId == R.id.edit_bar_menu_add_goal) {
             var dialogFragment = CreateGoalDialogFragment.newInstance();
             dialogFragment.show(goalList.getParentFragmentManager(), "CreateGoalDialogFragment");
-            return true;
-        });
+        }
+        else if(itemId == R.id.edit_bar_menu_edit_date){
+            activityModel.dayIncrement();
+            activityModel.rollOver();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.editgoal, menu);
         return true;
     }
 }
--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Date.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Date.java
@@ -1,11 +1,39 @@
 package edu.ucsd.cse110.successorator.lib.domain;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Objects;
 
 import edu.ucsd.cse110.successorator.lib.util.MutableSubject;
+import edu.ucsd.cse110.successorator.lib.util.SimpleSubject;
+import edu.ucsd.cse110.successorator.lib.util.Subject;
 
-public class Date {
-    Calendar calendar;
-    MutableSubject<String> dateString;
+public class Date{
+    private Calendar calendar;
+    private String format = "E, MMM dd";
 
+    public Date(Calendar calendar){
+        this.calendar = calendar;
+    }
+
+    public void dayIncrement(){
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Date date = (Date) o;
+        return Objects.equals(calendar, date.calendar) && Objects.equals(format, date.format);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(calendar, format);
+    }
 }
