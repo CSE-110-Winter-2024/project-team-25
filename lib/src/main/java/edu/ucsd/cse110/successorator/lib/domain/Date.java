163ea1a1056fe75ca485f75bafe435caf6b9ee04
Ziyue Liu <zil085@ucsd.edu>
2024-02-17 19:16:54 -0800

    add date class

---
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Date.java
@@ -0,0 +1,11 @@
+package edu.ucsd.cse110.successorator.lib.domain;
+
+import java.util.Calendar;
+
+import edu.ucsd.cse110.successorator.lib.util.MutableSubject;
+
+public class Date {
+    Calendar calendar;
+    MutableSubject<String> dateString;
+
+}
