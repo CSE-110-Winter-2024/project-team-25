674da1ccb81fd00996a3bf8cad59bb42cf5105a8
Andrew Wang <wangandrewwang88@gmail.com>
2024-03-11 18:00:03 -0700

    Changed behavior to respect day of week and week of month

--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Recurrence.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Recurrence.java
@@ -11,7 +11,7 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Recurrence implements Serializable {
+public class Recurrence implements Serializable, Cloneable {
     private @NonNull Date firstOccurrence;
     private @NonNull Date nextOccurrence;
     private @Nullable Date endDate;
@@ -30,7 +30,12 @@ public class Recurrence implements Serializable {
         if(index != -1) {
             nextDate.getCalendar().add(Calendar.YEAR, datePattern.get(index).getYears());
             nextDate.getCalendar().add(Calendar.MONTH, datePattern.get(index).getMonths());
-            nextDate.getCalendar().add(Calendar.DAY_OF_MONTH, datePattern.get(index).getDays());
+            if(datePattern.get(index).getDays() != 0) {
+                nextDate.getCalendar().add(Calendar.DAY_OF_MONTH, datePattern.get(index).getDays());
+            } else {
+              nextDate.getCalendar().set(Calendar.WEEK_OF_MONTH, nextOccurrence.getCalendar().get(Calendar.WEEK_OF_MONTH));
+              nextDate.getCalendar().set(Calendar.DAY_OF_WEEK, nextOccurrence.getCalendar().get(Calendar.DAY_OF_WEEK));
+            }
             nextOccurrence = nextDate;
         }
         index = (index + 1) % datePattern.size();
@@ -42,7 +47,27 @@ public class Recurrence implements Serializable {
     public Date getNextOccurrence() {
         return nextOccurrence;
     }
+    public boolean isFutureRecurrence(Date date) {
+        Recurrence clone = this.clone();
+        while(clone.nextOccurrence.getCalendar().before(date.getCalendar())) {
+            clone.applyRecurrence();
+        }
+        return clone.nextOccurrence.getCalendar().equals(date.getCalendar());
+    }
 
+    @Override
+    public Recurrence clone() {
+        try {
+            Recurrence clone = (Recurrence) super.clone();
+            clone.firstOccurrence = firstOccurrence.clone();
+            clone.nextOccurrence = nextOccurrence.clone();
+            clone.index = index;
+            clone.datePattern = new ArrayList<>(datePattern);
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
